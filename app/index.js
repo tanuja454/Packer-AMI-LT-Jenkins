08190f79af66b0cc47da46f14990b30c4b8b731a
tanuja454 <chennapragadalaxminagatanuja@gmail.com>
2025-05-31 16:22:37 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.get('/', (req, res) => {
   res.send('Hello from Node.js app running on custom AMI!');
